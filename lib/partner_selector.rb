2196581fb2433f4011991571f4c1c3b8c85339ee
Yury Romanov <yurras@Yurys-MacBook-Pro.local>
2020-08-08 16:33:59 +0200

    Join existing 3-rd employee to the mystery pair when needed

--- lib/partner_selector.rb
+++ lib/partner_selector.rb
@@ -1,34 +1,55 @@
 class PartnerSelector
-  attr_reader :employees, :pairs, :history
+  attr_reader :employees, :pairs, :history, :list_of_partners
 
-  def initialize(employees, departments, history)
+  def initialize(employees, history)
     @employees = employees.clone
-    @departments = departments || []
     @history = HistoryChecker.new(history)
     @pairs = []
   end
 
   def perform
     generate_partners
+    fix_edge_cases
+    list_of_partners
   end
 
   private
 
   def generate_partners
-    (0..pair_count).map do |index|
+    @list_of_partners = (0..pair_count).map do |index|
       partner1 = employees.shift
-      partner2 = employees.detect { |id2| history.none?(partner1, id2) }
+      partner2 = employees.detect { |partner| history.none?(partner1.id, partner.id) }
+
+      employees.push(partner1) && next if partner2.nil?
+
       employees.delete(partner2)
 
-      if index.eql?(pair_count) && employees.present?
-        [partner1, partner2, employees.last].compact
-      else
-        [partner1, partner2]
+      [partner1, partner2]
+    end.compact
+  end
+
+  def fix_edge_cases
+    if employees.count.eql?(2)
+      @list_of_partners << employees
+    else
+      join_to_existing_pair
+    end if employees.present?
+  end
+
+  def join_to_existing_pair
+    list_of_partners.each do |partners|
+      group = partners + [employees.last]
+      departments = group.map(&:department_id).uniq
+      different_departments = departments.count.eql?(group.count)
+
+      if different_departments
+        partners << employees.last
+        break
       end
     end
   end
 
   def pair_count
-    @pair_count ||= (@employees.length / 2) - 1
+    @pair_count ||= (employees.length / 2) - 1
   end
 end
--- spec/lib/partner_selector_spec.rb
+++ spec/lib/partner_selector_spec.rb
@@ -3,25 +3,14 @@ require 'rails_helper'
 describe PartnerSelector, type: :model do
   describe '#perform' do
     # e = employee
-    let(:e1){ 1 }
-    let(:e2){ 2 }
-    let(:e3){ 3 }
-    let(:e4){ 4 }
-    let(:e5){ 5 }
-    let(:e6){ 6 }
-    # d = department
-    let(:d1) { 1 }
-    let(:d2) { 2 }
-    let(:d3) { 3 }
-    let(:departments) do
-      {
-        d1 => [e1],
-        d2 => [e2],
-        d3 => [e3, e4],
-      }
-    end
+    let(:e1){ FactoryBot.build_stubbed(:employee, id: 1, department_id: 1) }
+    let(:e2){ FactoryBot.build_stubbed(:employee, id: 2, department_id: 2) }
+    let(:e3){ FactoryBot.build_stubbed(:employee, id: 3, department_id: 3) }
+    let(:e4){ FactoryBot.build_stubbed(:employee, id: 4, department_id: 4) }
+    let(:e5){ FactoryBot.build_stubbed(:employee, id: 5, department_id: 5) }
+    let(:e6){ FactoryBot.build_stubbed(:employee, id: 6, department_id: 6) }
     let(:history) { [] }
-    subject { described_class.new(employees, departments, history).perform }
+    subject { described_class.new(employees, history).perform }
 
     context 'for 2 employees' do
       let(:employees) { [e1, e2] }
@@ -41,46 +30,59 @@ describe PartnerSelector, type: :model do
       it { is_expected.to eq([[e1, e2], [e3, e4]]) }
 
       context 'when e1 and e2 were partners' do
-        let(:history) { [[e1, e2]] }
+        let(:history) { [[e1.id, e2.id]] }
 
         it { is_expected.to eq([[e1, e3], [e2, e4]]) }
       end
 
       context 'when e2 and e3 were partners' do
-        let(:history) { [[e2, e3]] }
+        let(:history) { [[e2.id, e3.id]] }
 
         it { is_expected.to eq([[e1, e2], [e3, e4]]) }
       end
 
-      # edge case!
+      # edge case #1
       context 'when e3 and e4 were partners' do
-        let(:history) { [[e3, e4]] }
+        let(:history) { [[e3.id, e4.id]] }
 
-        it { is_expected.to eq([[e1, e2], [e3, e4]]) }
+        it { is_expected.to eq([[e1, e2], [e4, e3]]) }
+      end
+
+      # edge case #2
+      context 'when all people were partners' do
+        let(:history) { [[e1.id, e2.id, e3.id, e4.id]] }
+
+        it { is_expected.to eq([[e2, e1], [e3, e4]]) }
       end
     end
 
     context 'for 5 employees' do
       let(:employees) { [e1, e2, e3, e4, e5] }
 
-      it { is_expected.to eq([[e1, e2], [e3, e4, e5]]) }
+      it { is_expected.to eq([[e1, e2, e5], [e3, e4]]) }
 
       context 'when e1 and e2 were partners' do
-        let(:history) { [[e1, e2]] }
+        let(:history) { [[e1.id, e2.id]] }
 
-        it { is_expected.to eq([[e1, e3], [e2, e4, e5]]) }
+        it { is_expected.to eq([[e1, e3, e5], [e2, e4]]) }
       end
 
       context 'when [e1, e2] and [e1, e3] were partners' do
-        let(:history) { [[e1, e2], [e1, e3]] }
+        let(:history) { [[e1.id, e2.id], [e1.id, e3.id]] }
 
-        it { is_expected.to eq([[e1, e4], [e2, e3, e5]]) }
+        it { is_expected.to eq([[e1, e4, e5], [e2, e3]]) }
       end
 
       context 'when e3 and e4 were partners' do
-        let(:history) { [[e3, e4]] }
+        let(:history) { [[e3.id, e4.id]] }
 
-        it { is_expected.to eq([[e1, e2], [e3, e5, e4]]) }
+        it { is_expected.to eq([[e1, e2, e4], [e3, e5]]) }
+
+        context 'and e1 and e4 have the same department' do
+          before { e4.department_id = e1.department_id }
+
+          it { is_expected.to eq([[e1, e2], [e3, e5, e4]]) }
+        end
       end
     end
 
@@ -90,25 +92,25 @@ describe PartnerSelector, type: :model do
       it { is_expected.to eq([[e1, e2], [e3, e4], [e5, e6]]) }
 
       context 'when e1 and e2 were partners' do
-        let(:history) { [[e1, e2]] }
+        let(:history) { [[e1.id, e2.id]] }
 
         it { is_expected.to eq([[e1, e3], [e2, e4], [e5, e6]]) }
       end
 
       context 'when e1, e2 and e3 were partners' do
-        let(:history) { [[e1, e2, e3]] }
+        let(:history) { [[e1.id, e2.id, e3.id]] }
 
         it { is_expected.to eq([[e1, e4], [e2, e5], [e3, e6]]) }
       end
 
       context 'when [e1, e2], [e1, e3] were partners' do
-        let(:history) { [[e1, e2], [e1, e3]] }
+        let(:history) { [[e1.id, e2.id], [e1.id, e3.id]] }
 
         it { is_expected.to eq([[e1, e4], [e2, e3], [e5, e6]]) }
       end
 
       context 'when [e1, e2], [e1, e3], [e1, e4] were partners' do
-        let(:history) { [[e1, e2], [e1, e3], [e1, e4]] }
+        let(:history) { [[e1.id, e2.id], [e1.id, e3.id], [e1.id, e4.id]] }
 
         it { is_expected.to eq([[e1, e5], [e2, e3], [e4, e6]]) }
       end
