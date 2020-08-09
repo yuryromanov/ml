1d57e550c0d7231a3dc8a943ff3bfe27282ad8bb
Yury Romanov <yurras@Yurys-MacBook-Pro.local>
2020-08-09 19:48:24 +0200

    Add some validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,8 @@ class Employee < ApplicationRecord
   has_many :lunch_partners
 
   scope :available_for_lunch, -> { where(available_for_lunch: true) }
+
+  validates :first_name, length: {maximum: 255}, allow_blank: false
+  validates :last_name, length: {maximum: 255}, allow_blank: false
+  validates :email, uniqueness: true
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 describe Employee, type: :model do
-  it { expect(true).to eq(true) }
+
+  ['first_name', 'last_name'].each do |field|
+    describe "##{field}" do
+      let(:employee) { FactoryBot.build_stubbed(:employee) }
+
+      context 'with empty value' do
+        before { employee.send("#{field}=", '') }
+
+        it { expect(employee).to be_invalid }
+      end
+
+      context 'with length 256 character' do
+        before { employee.send("#{field}=", 'a' * 256) }
+
+        it { expect(employee).to be_invalid }
+      end
+    end
+  end
+
+  describe "email" do
+    let(:email) { 'aaa@bbb.com' }
+    let!(:employee1) { FactoryBot.create(:employee, email: email) }
+
+    context 'for another employee with the same email' do
+      let(:employee2) { FactoryBot.build(:employee, email: email) }
+
+      it { expect(employee2).to be_invalid }
+    end
+  end
 end
